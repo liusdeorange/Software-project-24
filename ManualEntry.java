6099959e6f185dd3d9841d6ae056c52d3dc7db33
Mhs-at-bupt <sunminghao2004@bupt.edu.cn>
2025-04-17 15:08:35 +0800

    Delete ManualEntry.java

--- ManualEntry.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class ManualEntry {
-    public Entry createManualEntry(String date, double amount, String category, String description) {
-        return new Entry(date, amount, category, description);
-    }
-}
