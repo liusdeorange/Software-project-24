bced3f512d4e15efc15d1e6ff4d94905b5e7d12a
dont-lose-hair <2692724595@qq.com>
2025-04-17 22:32:13 +0800

    2025/4/17/22:32-陈柏良-整合雷镐铭制作的BaseUI界面

--- Account-Book/src/main/java/test/testBaseUi.java
+++ Account-Book/src/main/java/test/testBaseUi.java
@@ -1,8 +1,12 @@
 package test;
 
-
-
+import view.Impl.BaseUiImpl;
 
 public class testBaseUi {
-
+    public static void main(String[] args) {
+        // 创建并显示主窗口
+        BaseUiImpl baseUi = new BaseUiImpl();
+        baseUi.BaseWindow();
+    }
 }
+
